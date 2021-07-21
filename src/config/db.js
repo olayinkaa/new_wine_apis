a78d5c8c087b6e98940a98b318017efb8bc19049
Olayinka Ibrahim <olayinka.ibrahim@etranzact.com>
2021-07-21 12:43:35 +0100

    first commit

---
+++ src/config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true,
+            useCreateIndex:true,
+            useFindAndModify:true
+        })
+        console.log("MongoDB connection successful")
+    } catch (error) {
+        // console.log(error)
+        console.log("MongoDB connection Failed")
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB;
